4d358e0d679f8430246f7688ce87688e0b784a7d
Michael R. Hines <michael@hinespot.com>
2018-02-13 22:43:41 -0600

    Need to also update prov_cloud_ip

--- util/openvpn/client_connected.sh
+++ util/openvpn/client_connected.sh
@@ -28,7 +28,7 @@ echo "client connected $(date) params: $@" >> $logpath
 # reason being here is that PENDING will have been deleted back when the VM's
 # post_attach operations have already completed and CloudBench will have already
 # populated the 'real' object, in which case we have to update that object as well.
-(bash -c "sleep 5; redis-cli -h SERVER_BOOTSTRAP -n OSCI_DBID -p OSCI_PORT hset TEST_USER:CLOUD_NAME:VM:PENDING:UUID cloud_init_vpn $VPNIP; exists=\$(redis-cli --raw -h SERVER_BOOTSTRAP -n OSCI_DBID -p OSCI_PORT hexists TEST_USER:CLOUD_NAME:VM:UUID cloud_init_vpn); if [ \$exists == 1 ] ; then redis-cli -h SERVER_BOOTSTRAP -n OSCI_DBID -p OSCI_PORT hset TEST_USER:CLOUD_NAME:VM:UUID cloud_init_vpn $VPNIP; fi" &)
+(bash -c "sleep 5; redis-cli -h SERVER_BOOTSTRAP -n OSCI_DBID -p OSCI_PORT hset TEST_USER:CLOUD_NAME:VM:PENDING:UUID cloud_init_vpn $VPNIP; exists=\$(redis-cli --raw -h SERVER_BOOTSTRAP -n OSCI_DBID -p OSCI_PORT hexists TEST_USER:CLOUD_NAME:VM:UUID cloud_init_vpn); if [ \$exists == 1 ] ; then redis-cli -h SERVER_BOOTSTRAP -n OSCI_DBID -p OSCI_PORT hset TEST_USER:CLOUD_NAME:VM:UUID cloud_init_vpn $VPNIP; redis-cli -h SERVER_BOOTSTRAP -n OSCI_DBID -p OSCI_PORT hset TEST_USER:CLOUD_NAME:VM:UUID prov_cloud_ip $VPNIP; fi" &)
 
 # Run cloudbench's cloud-agnostic userdata later. Backwards compatible with VPN_ONLY = False
 (/tmp/userscript.sh &)
