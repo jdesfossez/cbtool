592040687074e0529253d55b458489ba43bd3be7
Julien Desfossez <jdesfossez@digitalocean.com>
2019-02-22 15:03:34 -0500

    Add a bunch of mysql tunings and PMM client support
    Signed-off-by: Julien Desfossez <jdesfossez@digitalocean.com>

--- scripts/tpcc/cb_reset_mysql.sh
+++ scripts/tpcc/cb_reset_mysql.sh
@@ -21,14 +21,42 @@ source $(echo $0 | sed -e "s/\(.*\/\)*.*/\1.\//g")/cb_common.sh
 
 MYSQL_CONF_FILE=`get_my_ai_attribute_with_default mysql_conf_file /etc/mysql/mysql.conf.d/mysqld.cnf`
 MYSQL_RAM_PERCENTAGE=`get_my_ai_attribute_with_default mysql_ram_percentage 70`
+MYSQL_INNODB_IO_CAPACITY=`get_my_ai_attribute_with_default mysql_innodb_io_capacity 200`
+MYSQL_INNODB_LOG_FILE_SIZE=`get_my_ai_attribute_with_default mysql_innodb_log_file_size 48M`
+MYSQL_QUERY_CACHE_SIZE=`get_my_ai_attribute_with_default mysql_query_cache_size 16M`
+MYSQL_TMP_TABLE_SIZE=`get_my_ai_attribute_with_default mysql_tmp_table_size 16M`
+MYSQL_TABLE_OPEN_CACHE=`get_my_ai_attribute_with_default mysql_table_open_cache 2000`
 
 SUDO_CMD=`which sudo`
+
+RESTART_MYSQL=0
+
+update_mysql_setting()
+{
+	SETTING=$1
+	VALUE=$2
+
+	if grep "${SETTING} = ${VALUE}" ${MYSQL_CONF_FILE} >/dev/null; then
+		return
+	fi
+
+	syslog_netcat "Updating ${SETTING} to ${VALUE}"
+	${SUDO_CMD} sed -i 's/${SETTING}.*/${SETTING} = ${VALUE}' ${MYSQL_CONF_FILE}
+	RESTART_MYSQL=1
+}
 
 # Check if mysql has the right cache size or update it.
 kb=$(cat /proc/meminfo  | sed -e "s/ \+/ /g" | grep MemTotal | cut -d " " -f 2) 
 mb=$(echo "$kb / 1024 * ${MYSQL_RAM_PERCENTAGE} / 100" | bc)
-if ! grep "innodb_buffer_pool_size = ${mb}M" ${MYSQL_CONF_FILE} >/dev/null; then
-    syslog_netcat "Updating MySQL buffer pool size to ${mb}M..."
-	${SUDO_CMD} su -c "sed -i 's/innodb_buffer_pool_size.*/innodb_buffer_pool_size = ${mb}M/' /etc/mysql/mysql.conf.d/mysqld.cnf"
+
+update_mysql_setting innodb_buffer_pool_size ${mb}M
+update_mysql_setting mysql_innodb_io_capacity ${MYSQL_INNODB_IO_CAPACITY}
+update_mysql_setting mysql_innodb_log_file_size ${MYSQL_INNODB_LOG_FILE_SIZE}
+update_mysql_setting mysql_query_cache_size ${MYSQL_QUERY_CACHE_SIZE}
+update_mysql_setting mysql_tmp_table_size ${MYSQL_TMP_TABLE_SIZE}
+update_mysql_setting mysql_table_open_cache ${MYSQL_TABLE_OPEN_CACHE}
+
+if test ${RESTART_MYSQL} = 1; then
 	service mysql restart
+	syslog_netcat "Restarting MySQL after setting update"
 fi
--- scripts/tpcc/cb_restart_mysql.sh
+++ scripts/tpcc/cb_restart_mysql.sh
@@ -26,6 +26,13 @@ MYSQL_NONROOT_PASSWORD=`get_my_ai_attribute_with_default mysql_nonroot_password 
 MYSQL_DATA_DIR=`get_my_ai_attribute_with_default mysql_data_dir /sysbench`
 MYSQL_CONF_FILE=`get_my_ai_attribute_with_default mysql_conf_file /etc/mysql/mysql.conf.d/mysqld.cnf`
 MYSQL_RAM_PERCENTAGE=`get_my_ai_attribute_with_default mysql_ram_percentage 70`
+MYSQL_INNODB_IO_CAPACITY=`get_my_ai_attribute_with_default mysql_innodb_io_capacity 200`
+MYSQL_INNODB_LOG_FILE_SIZE=`get_my_ai_attribute_with_default mysql_innodb_log_file_size 48M`
+MYSQL_QUERY_CACHE_SIZE=`get_my_ai_attribute_with_default mysql_query_cache_size 16M`
+MYSQL_TMP_TABLE_SIZE=`get_my_ai_attribute_with_default mysql_tmp_table_size 16M`
+MYSQL_TABLE_OPEN_CACHE=`get_my_ai_attribute_with_default mysql_table_open_cache 2000`
+MYSQL_ENABLE_PMM_CLIENT=`get_my_ai_attribute_with_default mysql_enable_pmm_client False`
+MYSQL_PMM_SERVER=`get_my_ai_attribute_with_default mysql_pmm_server 0.0.0.0:80`
 
 SHORT_HOSTNAME=$(uname -n| cut -d "." -f 1)
 NETSTAT_CMD=`which netstat`
@@ -47,10 +54,25 @@ fi
 
 ${SUDO_CMD} sed -i "s^bind-address.*^bind-address            = $my_ip_addr^g" ${MYSQL_CONF_FILE}
 
+${SUDO_CMD} sed -i '/innodb_io_capacity/d' ${MYSQL_CONF_FILE}
+${SUDO_CMD} sed -i '/innodb_log_file_size/d' ${MYSQL_CONF_FILE}
+${SUDO_CMD} sed -i '/innodb_buffer_pool_size/d' ${MYSQL_CONF_FILE}
+${SUDO_CMD} sed -i '/query_cache_size/d' ${MYSQL_CONF_FILE}
+${SUDO_CMD} sed -i '/tmp_table_size/d' ${MYSQL_CONF_FILE}
+${SUDO_CMD} sed -i '/table_open_cache/d' ${MYSQL_CONF_FILE}
+${SUDO_CMD} sed -i '/innodb_buffer_pool_dump_at_shutdown/d' ${MYSQL_CONF_FILE}
+${SUDO_CMD} sed -i '/innodb_buffer_pool_load_at_startup/d' ${MYSQL_CONF_FILE}
+
 # Set mysql's memory cache size to be a percentage of main memory
 kb=$(cat /proc/meminfo  | sed -e "s/ \+/ /g" | grep MemTotal | cut -d " " -f 2)
 mb=$(echo "$kb / 1024 * ${MYSQL_RAM_PERCENTAGE} / 100" | bc)
 ${SUDO_CMD} su -c "echo 'innodb_buffer_pool_size = ${mb}M' >> ${MYSQL_CONF_FILE}"
+
+${SUDO_CMD} su -c "echo 'innodb_io_capacity = ${MYSQL_INNODB_IO_CAPACITY}' >> ${MYSQL_CONF_FILE}"
+${SUDO_CMD} su -c "echo 'innodb_log_file_size = ${MYSQL_INNODB_LOG_FILE_SIZE}' >> ${MYSQL_CONF_FILE}"
+${SUDO_CMD} su -c "echo 'query_cache_size = ${MYSQL_QUERY_CACHE_SIZE}' >> ${MYSQL_CONF_FILE}"
+${SUDO_CMD} su -c "echo 'tmp_table_size = ${MYSQL_TMP_TABLE_SIZE}' >> ${MYSQL_CONF_FILE}"
+${SUDO_CMD} su -c "echo 'table_open_cache = ${MYSQL_TABLE_OPEN_CACHE}' >> ${MYSQL_CONF_FILE}"
 
 if [[ $(${SUDO_CMD} ls /etc/apparmor.d/tunables/ | grep -c alias) -ne 0 ]]
 then
@@ -68,6 +90,26 @@ do
 	if [ $? -eq 0 ]
 	then
 		syslog_netcat "MySQL restarted succesfully on $SHORT_HOSTNAME - OK"
+		if test ${MYSQL_ENABLE_PMM_CLIENT} = 'True'; then
+			if ! grep Ubuntu /etc/lsb-release >/dev/null 2>&1; then
+				syslog_netcat "PMM Client installation only supported on Ubuntu"
+			else
+				${SUDO_CMD} sed -i '/innodb_monitor_enable/d' ${MYSQL_CONF_FILE}
+				${SUDO_CMD} sed -i '/performance_schema/d' ${MYSQL_CONF_FILE}
+				${SUDO_CMD} su -c "echo 'innodb_monitor_enable=all' >> ${MYSQL_CONF_FILE}"
+				${SUDO_CMD} su -c "echo 'performance_schema=ON' >> ${MYSQL_CONF_FILE}"
+				service mysql restart
+
+				wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
+				sudo dpkg -i percona-release_latest.generic_all.deb
+				sudo apt-get update
+				sudo apt-get -y install pmm-client
+				pmm-admin config --server ${MYSQL_PMM_SERVER}
+				pmm-admin add mysql --user root --password ${MYSQL_ROOT_PASSWORD} --create-user
+				pmm-admin add mysql --user root --password ${MYSQL_ROOT_PASSWORD} --query-source perfschema
+			fi
+		fi
+
 		provision_application_stop $START
 		exit 0
 	else 
